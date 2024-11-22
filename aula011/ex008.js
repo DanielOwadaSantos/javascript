b1b40483f7968611a1510cf71d150ac89aadda60
DanielOwadaSantos <0389dos@gmail.com>
2024-11-22 07:28:26 -0300

    atualização

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+console.log('O console funionou corretamente!')
+var vel = 61
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!!!')
+}
+console.log(`A velocidade do carro é ${vel}km/h.`)
+console.log('Dirija usando cinto de segurança.')
+
