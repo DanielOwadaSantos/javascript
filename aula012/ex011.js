401623077ff852e783dbca120ce170ebcb043876
DanielOwadaSantos <0389dos@gmail.com>
2024-11-27 22:17:29 -0300

    atualização

--- aula012/ex011.js
+++ aula012/ex011.js
@@ -1,7 +1,26 @@
-var país = 'Brasil'
-if (país != 'Brasil'){
-    console.log('Você é estangeiro')   
+/*var país = 'eua'
+if (país == 'Brasil'){
+    console.log('Você é brasileiro.')   
 }
 else{
-    console.log('Você é brasileiro')
+    console.log('Você é estangeiro.')
+}*/
+
+/*var idade = 17
+if (idade > 18){
+    console.log('Você é maior de idade.')
+}
+else{
+    console.log('Você é menor de idade.')
+}*/
+
+var idade = 88
+    if (idade < 16){
+    console.log('Você não pode votar.')
+} 
+else if(/*idade >= 16 &&*/ idade < 18 || idade > 65){
+    console.log('Voto opcional.')
+}
+else{
+    console.log('Voto obrigatório.')
 }
